e205cf19eece632c7a765e8d69bc47cc192e2d5f
nyanlathotep <pseudobyte@gmail.com>
2019-11-12 17:20:31 -0700

    updated merge and trim to use uxcore logging and messages

--- merge.py
+++ merge.py
@@ -4,39 +4,45 @@ from json import dump
 from collections import OrderedDict
 from tryload import tryload
 from clocky import time_24
+import uxcore
 
-### configuration
-
-fp = open('data\config.json')
-config = tryload(fp)
-fp.close()
-
-### sort input files
-
-files = sorted(argv[1:])
-
-### load first
-
-fp = open(files[0])
-data = tryload(fp, object_pairs_hook = OrderedDict)
-fp.close()
-
-### append remainder
-
-for i in files[1:]:
-  fp = open(i)
-  week = tryload(fp, object_pairs_hook = OrderedDict)
+try:
+  ### configuration
+  fp = open('data\\config.json')
+  config = tryload(fp)
   fp.close()
-  for i in week:
-    data[i] = week[i]
 
-### output
+  ### sort input files
 
-for day in data:
-  for event in data[day]['classes']:
-    event['begin'] = time_24(event['begin'])
-    event['end'] = time_24(event['end'])
+  files = sorted(argv[1:])
 
-fp = open('schedule.json', mode = 'w')
-dump(data, fp, indent=2, separators=(',', ': '))
-fp.close()
+  ### load first
+
+  fp = open(files[0])
+  data = tryload(fp, object_pairs_hook = OrderedDict)
+  fp.close()
+
+  ### append remainder
+
+  for i in files[1:]:
+    fp = open(i)
+    week = tryload(fp, object_pairs_hook = OrderedDict)
+    fp.close()
+    for i in week:
+      data[i] = week[i]
+
+  ### output
+
+  for day in data:
+    for event in data[day]['classes']:
+      event['begin'] = time_24(event['begin'])
+      event['end'] = time_24(event['end'])
+
+  output_path = 'schedule.json'
+  fp = open(output_path, mode = 'w')
+  dump(data, fp, indent=2, separators=(',', ': '))
+  fp.close()
+  uxcore.display_success(['SUCCESS: converted file, outputted:', '         {}'.format(output_path)])
+except:
+  log_path = uxcore.write_log('conv', files=[argv[1]])
+  uxcore.display_error(['ERROR: Failed to convert file, error data written:', '       {}'.format(log_path)])
--- trim.py
+++ trim.py
@@ -4,63 +4,69 @@ from json import dump
 from collections import OrderedDict
 from tryload import tryload
 from clocky import time_12, time_24
+import uxcore
 
-### configuration
+try:
+  ### configuration
 
-fp = open('data\config.json')
-config = tryload(fp)
-fp.close()
+  fp = open('data\\config.json')
+  config = tryload(fp)
+  fp.close()
 
-### input
+  ### input
 
-fp = open(argv[1], mode = 'r')
-week = tryload(fp, object_pairs_hook = OrderedDict)
-fp.close()
+  fp = open(argv[1], mode = 'r')
+  week = tryload(fp, object_pairs_hook = OrderedDict)
+  fp.close()
 
-### convert times to 24 hour
+  ### convert times to 24 hour
 
-for day in week:
-  for event in week[day]['classes']:
-    event['begin'] = time_24(event['begin'])
-    event['end'] = time_24(event['end'])
+  for day in week:
+    for event in week[day]['classes']:
+      event['begin'] = time_24(event['begin'])
+      event['end'] = time_24(event['end'])
 
-### set margins
+  ### set margins
 
-for i in week:
-  classes = []
-  for x in week[i]["classes"]:
-    begin = x["begin"].split(":")
-    begin = int(begin[0])*60 + int(begin[1])
-    end = x["end"].split(":")
-    end = int(end[0])*60 + int(end[1])
-    classes += [(begin,end)]
-  fitness = {}
-  for sm in range(30, 480, 15):
-    for em in range(0, 240, 15):
-      f = 0
-      for time in range(480+60*(i==5), 1261-360*(i==5), 5):
-        stuff = sum([1 for x in classes if time >= (x[0] - sm) and time <= (x[1] + em)])
-        if stuff > f:
-          f = stuff
-      fitness[(sm,em)] = f
-  good = {x:fitness[x] for x in fitness if fitness[x] <= 13}
-  sel = []
-  for x in good:
-    val = abs(0.5 - x[1]/(x[0]-59.)) - 2*(x[0]>60) - 2*(x[0]>90) - x[0]/30. - x[1]/30.
-    sel += [(x, val)]
-  sel.sort(key=lambda x: x[1])
-  week[i]["start_margin"] = sel[0][0][0]
-  week[i]["end_margin"] = sel[0][0][1]
+  for i in week:
+    classes = []
+    for x in week[i]["classes"]:
+      begin = x["begin"].split(":")
+      begin = int(begin[0])*60 + int(begin[1])
+      end = x["end"].split(":")
+      end = int(end[0])*60 + int(end[1])
+      classes += [(begin,end)]
+    fitness = {}
+    for sm in range(30, 480, 15):
+      for em in range(0, 240, 15):
+        f = 0
+        for time in range(480+60*(i==5), 1261-360*(i==5), 5):
+          stuff = sum([1 for x in classes if time >= (x[0] - sm) and time <= (x[1] + em)])
+          if stuff > f:
+            f = stuff
+        fitness[(sm,em)] = f
+    good = {x:fitness[x] for x in fitness if fitness[x] <= 13}
+    sel = []
+    for x in good:
+      val = abs(0.5 - x[1]/(x[0]-59.)) - 2*(x[0]>60) - 2*(x[0]>90) - x[0]/30. - x[1]/30.
+      sel += [(x, val)]
+    sel.sort(key=lambda x: x[1])
+    week[i]["start_margin"] = sel[0][0][0]
+    week[i]["end_margin"] = sel[0][0][1]
 
-### output
+  ### output
 
-time_mode = config['time_mode']
+  time_mode = config['time_mode']
 
-for day in week:
-  for event in week[day]['classes']:
-    event['begin'] = time_12(event['begin'], time_mode)
-    event['end'] = time_12(event['end'], time_mode)
+  for day in week:
+    for event in week[day]['classes']:
+      event['begin'] = time_12(event['begin'], time_mode)
+      event['end'] = time_12(event['end'], time_mode)
 
-fp = open(argv[1], mode = 'w')
-dump(week, fp, indent=2, separators=(',', ': '))
-fp.close()
+  fp = open(argv[1], mode = 'w')
+  dump(week, fp, indent=2, separators=(',', ': '))
+  fp.close()
+  uxcore.display_success(['SUCCESS: converted file, outputted:', '         {}'.format(argv[1])])
+except:
+  log_path = uxcore.write_log('conv', files=[argv[1]])
+  uxcore.display_error(['ERROR: Failed to convert file, error data written:', '       {}'.format(log_path)])
